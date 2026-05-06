07f64d861f78e3c7c3de5dc255d053a40be6d7f5
sujoy <fsyedhuq@uci.edu>
2026-05-05 18:47:15 -0700

    Include basename for github pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { ContactPage } from './pages/ContactPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/vibing-food-catering/">
       <CartProvider>
         <Routes>
           <Route element={<Layout />}>
