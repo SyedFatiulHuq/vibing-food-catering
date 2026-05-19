55c3044c43a524a12c92ede8293a5ae9711c1ef5
Syed Fatiul Huq <97715174+SyedFatiulHuq@users.noreply.github.com>
2026-05-19 14:16:42 -0700

    Update App.tsx to replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { OrdersProvider } from "./context/OrdersContext";
 import { DocumentTitle } from "./components/DocumentTitle";
@@ -14,7 +14,7 @@ import { ContactPage } from "./pages/ContactPage";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/vibing-food-catering/">
+    <HashRouter>
       <DocumentTitle />
       <CartProvider>
         <OrdersProvider>
@@ -33,6 +33,6 @@ export default function App() {
           </Routes>
         </OrdersProvider>
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
